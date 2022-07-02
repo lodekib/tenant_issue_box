62e4c7b43b6ba53c6a48447b6a02a56863db347c
Brian <lodekib@gmail.com>
2022-07-02 14:36:02 +0300

    initial commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Home() {
+  return (
+    <>
+      tests
+      </>
+      
+  );
+}
+
+ Home.layout = (page) => <Guest children={page} title={"Home"}/>
